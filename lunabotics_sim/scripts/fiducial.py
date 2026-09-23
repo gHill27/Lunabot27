c912ee480f7cd2d4c8b351b95e2e60629c1edbaf
Gavin Hill <gjhill@wpi.edu>
2026-09-23 14:42:03 -0400

    added basic structure to the fiducial.py init

--- lunabotics_sim/scripts/fiducial.py
+++ lunabotics_sim/scripts/fiducial.py
@@ -9,3 +9,13 @@ from rclpy.node import Node
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
 
+#Fiducial tracker node
+class FiducialTracker(Node):
+    def __init__(self):
+        super.__init__("fiducial_tracker")
+        self.bridge = CvBridge() #allows ROS2 <sensor_msgs/msg/Image> to be compatable with cv2 algorithms
+
+        self.create_subscription(Image, '/camera/image', self.rgb_callback, 10) #only rgb to track the marker
+
+    def rgb_callback(self, image:Image) -> None:
+        pass
